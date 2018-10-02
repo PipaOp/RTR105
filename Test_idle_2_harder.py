113969768876724748c1df478356b4ef63cf5c6f
user <dimasergej@gmail.com>
2018-10-02 16:04:40 +0300

    20181002_16_04

---
+++ Test_idle_2_harder.py
@@ -0,0 +1,13 @@
+x = 5
+if x == 5:
+    print("Equal")
+if x > 4:
+    print("Greater than 4")
+if x >= 5:
+    print("Grater than or Equals 5")
+if x < 6:
+    print("Less than 6")
+if x <= 5:
+    print("Less than or Equals 5")
+if x != 6:
+    print("No equal 6")
---
+++ dgr_20181002c.py
@@ -0,0 +1,61 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 5
+>>> if x < 10;
+SyntaxError: invalid syntax
+>>> if x < 10: print("Smaller")
+5
+SyntaxError: invalid syntax
+>>> if x > 20: print("Bigger")
+
+>>> print ("Finis")
+Finis
+>>> x = 5
+>>> if x < 10: print("Smaller") if x > 20: print("Bigger") print ("Finis)
+							      
+SyntaxError: invalid syntax
+>>> x = 5
+>>> if x < 10:
+	print("bigger")
+
+bigger
+>>> Test_idle.py
+
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    Test_idle.py
+NameError: name 'Test_idle' is not defined
+>>> 
+================== RESTART: /home/user/RTR105/Test_idle.py ==================
+Smaller
+>>> 
+================== RESTART: /home/user/RTR105/Test_idle.py ==================
+Smaller
+Finis
+>>> 
+============== RESTART: /home/user/RTR105/Test_idle_2_harder.py ==============
+Equal
+Greater than 4
+Grater than or Equals 5
+Less than 6
+Less than or Equals 5
+No equal 6
+>>> 
+================= RESTART: /home/user/RTR105/Test_idle_3.py =================
+Before 5
+Is 5
+Is Still 5
+Third 5
+Afterwards 5
+Before 6
+Afterwards
+>>> x = input("Enter")
+Enter5
+>>> x
+5
+>>> payperhour = input ("payperhour")
+payperhour2.75
+>>> type (payperhour)
+<type 'float'>
+>>> 
